d347d8814c37638e8814774dcca8d664e25bac9c
Dhionatã <xiyunagi@gmail.com>
2024-09-20 11:08:54 -0300

    mainTest() now have Assertions.assertDoesNotThrow{}; "metaFlacFile" have a default "metaflac.exe" in the test resources.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,30 +4,34 @@ import flac.ShellPropertyService
 import utils.FileSelector
 import utils.Logger
 import java.io.File
+import java.util.Date
 import javax.swing.JOptionPane
 
 fun main() {
-    val fileSelector = FileSelector()
+    try {
+        val logger = Logger("flac_processing_log ${Date().hashCode()}.txt")
 
-    val metaFlacFile = fileSelector.selectMetaFlacPath()
-    println("Selected metaflac.exe file: ${metaFlacFile.absolutePath}")
+        val fileSelector = FileSelector()
 
-    val musicDirectory = fileSelector.selectMusicDirectory()
-    println("Selected song directory: ${musicDirectory.absolutePath}")
+        val metaFlacFile = fileSelector.selectMetaFlacPath()
+        logger.log("Selected metaflac.exe file: ${metaFlacFile.absolutePath}")
 
-    val logger = Logger("flac_processing_log.txt")
+        val musicDirectory = fileSelector.selectMusicDirectory()
+        logger.log("Selected song directory: ${musicDirectory.absolutePath}")
 
+        val removedCoversFile = File(".", "removed_covers ${Date().hashCode()}.txt").absolutePath
 
-    val removedCoversFile = File(".", "removed_covers.txt").absolutePath
+        val metaFlacService = MetaFlacService(metaFlacFile.absolutePath, logger)
+        val shellPropertyService = ShellPropertyService()
+        val fileProcessor = FileProcessor(
+            metaFlacService, shellPropertyService, musicDirectory.absolutePath, logger,
+            removedCoversFile
+        )
 
-    val metaFlacService = MetaFlacService(metaFlacFile.absolutePath, logger)
-    val shellPropertyService = ShellPropertyService()
-    val fileProcessor = FileProcessor(
-        metaFlacService, shellPropertyService, musicDirectory.absolutePath, logger,
-        removedCoversFile
-    )
+        fileProcessor.processFlacFiles()
 
-    fileProcessor.processFlacFiles()
-
-    JOptionPane.showMessageDialog(null, "Finished")
+        JOptionPane.showMessageDialog(null, "Finished")
+    } catch (e: Exception) {
+        JOptionPane.showMessageDialog(null, "Finished with ERROR\n$e")
+    }
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,31 +1,40 @@
 import flac.FileProcessor
 import flac.MetaFlacService
 import flac.ShellPropertyService
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import utils.Logger
 import java.io.File
 import java.nio.file.Paths
+import java.util.Date
+import javax.swing.JOptionPane
 
 class MainTest {
+    val metaFlacFile = File("metaflac.exe")
+    val musicDirectory: File = File(Paths.get(System.getProperty("user.home"), "Music").toString())
 
     @Test
     fun mainTest() {
-        val metaFlacFile = File("metaflac.exe")
-        val musicDirectory: File = File(Paths.get(System.getProperty("user.home"), "Music").toString())
+        Assertions.assertDoesNotThrow {
+            try {
+                val logger = Logger("flac_processing_log_test ${Date().hashCode()}.txt")
 
-        val logger = Logger("flac_processing_log_test.txt")
+                val removedCoversFile = File("removed_covers_test ${Date().hashCode()}.txt")
 
-        val removedCoversFile = File("removed_covers_test.txt")
+                val metaFlacService = MetaFlacService(metaFlacFile.absolutePath, logger)
+                val shellPropertyService = ShellPropertyService()
 
-        val metaFlacService = MetaFlacService(metaFlacFile.absolutePath, logger)
-        val shellPropertyService = ShellPropertyService()
+                val fileProcessor = FileProcessor(
+                    metaFlacService, shellPropertyService, musicDirectory.absolutePath, logger,
+                    removedCoversFile.absolutePath
+                )
 
-        val fileProcessor = FileProcessor(
-            metaFlacService, shellPropertyService, musicDirectory.absolutePath, logger,
-            removedCoversFile.absolutePath
-        )
+                fileProcessor.processFlacFiles()
 
-        fileProcessor.processFlacFiles()
+                JOptionPane.showMessageDialog(null, "Finished")
+            } catch (e: Exception) {
+                JOptionPane.showMessageDialog(null, "Finished with ERROR\n$e")
+            }
+        }
     }
-
 }
